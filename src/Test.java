ef50a2d7c0ded0ab3cce682cff7ec4780b90f5e5
abhinav0948 <127231135+abhinav0948@users.noreply.github.com>
2024-08-31 19:16:36 +0530

    Delete src/Test.java

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("My second code commit");
-        System.out.println("my name is Abhinav");
-        System.out.println("Hey there");
-    }
-}
